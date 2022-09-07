7a30d71d7fa94a81f16711bb239ca568bb1116ce
BonghoCha <37327313+BonghoCha@users.noreply.github.com>
2022-09-07 22:06:05 +0900

    add: PlayerInfo 클래스 추가
    - 공격력,이동속도,치명타공격력,치명타확률,발사빈도

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public static class PlayerInfo
+{
+    public static float Power = 10f;
+    public static float Speed = 1000f;
+
+    public static float CriticalPower = 2f;
+    public static float Critical = 0.5f;
+
+    public static float Firerate = 0.25f;
+}
