a2813beaf12acbb715713c7216122d3f6b2610fb
BonghoCha <37327313+BonghoCha@users.noreply.github.com>
2022-12-23 00:22:29 +0900

    update: BGM 재생 기능 추가

--- Assets/Scripts/Common/SoundManager.cs
+++ Assets/Scripts/Common/SoundManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class ClipData
@@ -16,10 +18,24 @@ public class ClipData
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager instance = null;
+    
     [SerializeField] private AudioSource BGM;
     [SerializeField] private AudioSource Effect;
 
     private Dictionary<string, ClipData> clipDictionary = new Dictionary<string, ClipData>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
     public void SoundPlay(string name)
     {
@@ -31,8 +47,35 @@ public class SoundManager : MonoBehaviour
             clipDictionary.Add(name, clipData);
         }
 
-        Effect.clip = clipData.audioClip;
-        
-        Effect.Play();
+        if (name.Contains("BGM"))
+        {
+            Sequence bgmSequence = DOTween.Sequence();
+
+            var fadeDelay = 0f;
+            if (BGM.isPlaying)
+            {
+                fadeDelay = 0.5f;
+            }
+
+            bgmSequence.Append(BGM.DOFade(0, fadeDelay).OnComplete(() =>
+            {
+                BGM.clip = clipData.audioClip;
+                BGM.Play();
+            }));
+            bgmSequence.Append(BGM.DOFade(1, fadeDelay));
+
+            bgmSequence.Play();
+        }
+        else
+        {
+            Effect.PlayOneShot(clipData.audioClip);
+        }
+    }
+
+    public void Clear()
+    {
+        BGM.Stop();
+        Effect.Stop();
+        DOTween.KillAll();
     }
 }
